d50ae3b00d39eeaa02ddd277d0539e2ebb6d00a9
Rory <roryjamesgt@gmail.com>
2026-06-08 21:17:05 +1000

    cart price js

--- script.js
+++ script.js
@@ -3,22 +3,37 @@ const quantityDisplay = document.getElementById("quantity");
 const plusBtn = document.getElementById("plusbtn");
 const minusBtn = document.getElementById("minusbtn");
 const pdreamPrice = document.getElementById("pdreamprice");
+const mbPrice = document.getElementById("mbprice")
 
 let quantity = 1;
 
 plusBtn.addEventListener("click", () => {
     quantity++;
+
     quantityDisplay.textContent = quantity;
-    pdreamPrice = quantity*70;
-    pdreamPrice.textContent = "$"+quantity*70+".00 AUD";
+
+    if (pdreamPrice) {
+        pdreamPrice.textContent = "$" + (quantity * 70) + ".00 AUD";
+    }
+
+    if (mbPrice) {
+        mbPrice.textContent = "$" + (quantity * 95) + ".00 AUD";
+    }
 });
 
 minusBtn.addEventListener("click", () => {
+
     if (quantity > 1) {
         quantity--;
+
         quantityDisplay.textContent = quantity;
-        pdreamPrice = quantity*70;
-        pdreamPrice.textContent = "$"+quantity*70+".00 AUD";
+
+        if (pdreamPrice) {
+            pdreamPrice.textContent = "$" + (quantity * 70) + ".00 AUD";
+        }
+
+        if (mbPrice) {
+            mbPrice.textContent = "$" + (quantity * 95) + ".00 AUD";
+        }
     }
-    
 });
